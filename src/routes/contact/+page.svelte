ec91f658034fd33a1c5866041cec13d4e9d25be6
fdiengott <freddy.diengott@proton.me>
2023-07-27 09:33:25 -0400

    put contact grid into each loop

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,15 +7,21 @@
 	let isPageLoaded = false;
 	onMount(() => (isPageLoaded = true));
 
-	const NUM_ROWS = 7;
-	const NUM_CELLS = NUM_ROWS * 2;
-	const transitionParams = Array.from({ length: NUM_CELLS + 1 }, (_, i) => ({
-		delay: 200 + i * 50,
-		duration: 400,
+	const rows = [
+		{ label: 'Name', value: 'Freddy Diengott' },
+		{ label: 'Email', value: 'fdiengott@gmail.com' },
+		{ label: 'Location', value: 'Brooklyn, NY' },
+		{ label: 'Github', value: { link: 'https://github.com/fdiengott', text: 'fdiengott' } },
+		{ label: 'LinkedIn', value: { link: 'https://www.linkedin.com/in/freddiengott/', text: 'freddiengott' } },
+		{ label: 'Wellfound', value: { link: 'https://wellfound.com/u/freddy-diengott', text: 'freddy-diengott' } },
+		{ label: 'Resume', value: { link: resume, text: 'Link' } }
+	];
+
+	const defaultTransitionParams = {
+		duration: 600,
 		y: -50,
-		opacity: 0,
 		easing: quadOut
-	}));
+	};
 </script>
 
 <svelte:head>
@@ -27,55 +33,28 @@
 		<h1>Contact Me</h1>
 	</div>
 	{#if isPageLoaded}
-		<div transition:fly={transitionParams[0]}>
+		<div transition:fly={{ ...defaultTransitionParams, delay: 200 }}>
 			<div class="page-break" />
 		</div>
-		<table>
-			<tbody>
-				<tr>
-					<th transition:fly={transitionParams[1]}>Name</th>
-					<th transition:fly={transitionParams[2]}>Freddy Diengott</th>
-				</tr>
-				<tr>
-					<th transition:fly={transitionParams[3]}>Email</th>
-					<th transition:fly={transitionParams[4]}>fdiengott@gmail.com</th>
-				</tr>
-				<tr>
-					<th transition:fly={transitionParams[5]}>Location</th>
-					<th transition:fly={transitionParams[6]}>Brooklyn, NY</th>
-				</tr>
-				<tr>
-					<th transition:fly={transitionParams[7]}>Github</th>
-					<th transition:fly={transitionParams[8]}
-						><a href="https://github.com/fdiengott" target="_blank" rel="noopener noreferrer">fdiengott</a
-						></th
-					>
-				</tr>
-				<tr>
-					<th transition:fly={transitionParams[9]}>LinkedIn</th>
-					<th transition:fly={transitionParams[10]}
-						><a href="https://www.linkedin.com/in/freddiengott/" target="_blank" rel="noopener noreferrer"
-							>freddiengott</a
-						></th
-					>
-				</tr>
-				<tr>
-					<th transition:fly={transitionParams[11]}>Wellfound</th>
-					<th transition:fly={transitionParams[12]}>
-						<a href="https://wellfound.com/u/freddy-diengott" target="_blank" rel="noopener noreferrer">
-							freddy-diengott
-						</a>
-					</th>
-				</tr>
-				<tr>
-					<th transition:fly={transitionParams[13]}>Resume</th>
-					<th transition:fly={transitionParams[14]}
-						><a href={resume} target="_blank" rel="noopener noreferrer">Link</a></th
-					>
-				</tr>
-			</tbody>
-		</table>
 	{/if}
+	<table>
+		<tbody>
+			{#each rows as row, index}
+				{#if isPageLoaded}
+					<tr>
+						<th transition:fly={{ ...defaultTransitionParams, delay: 250 + index * 50 }}>{row.label}</th>
+						<th transition:fly={{ ...defaultTransitionParams, delay: 250 + (index + 1) * 50 }}>
+							{#if typeof row.value === 'string'}
+								{row.value}
+							{:else}
+								<a href={row.value.link} target="_blank" rel="noopener noreferrer">{row.value.text}</a>
+							{/if}
+						</th>
+					</tr>
+				{/if}
+			{/each}
+		</tbody>
+	</table>
 </article>
 
 <style>
