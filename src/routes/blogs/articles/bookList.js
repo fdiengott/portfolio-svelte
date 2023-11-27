80ba91a56e783217485b7a0475c5360d586bc411
fdiengott <freddy.diengott@proton.me>
2023-11-26 20:32:06 -0500

    add the new map to my book list

--- src/routes/blogs/articles/bookList.js
+++ src/routes/blogs/articles/bookList.js
@@ -3,6 +3,7 @@ const bookList = `
 
 I've been reading a lot of non-fiction lately, massively inspiring me and exploding my thinking. I'd like to share some of the most recent books here that have made a big impact on me in no particular order.
 
+- **The New Map: Energy, Climate, and the Clash of Nations** by Daniel Yergin
 - **Chip War: The fight for the world's most critical technology** by Chris Miller
 - **The Precipice: Existential Risk and the Future of Humanity** by Toby Ord
 - **Everyday Utopia: What 2,000 Years of Wild Experiments Can Teach Us About the Good Life** by Kristen Ghodsee
