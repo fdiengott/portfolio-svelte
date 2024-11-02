d814fef3d50986ce2b776fb91b29b98883baba76
fdiengott <freddy.diengott@proton.me>
2024-11-02 10:05:43 -0400

    add code syntax highlighting to articles

---
+++ src/routes/blogs/[slug]/Code.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Highlight from 'svelte-highlight';
+	import typescript from 'svelte-highlight/languages/typescript';
+	import atelierCaveLight from 'svelte-highlight/styles/atelier-cave-light';
+	export let text = 'text';
+</script>
+
+<svelte:head>
+	{@html atelierCaveLight}
+</svelte:head>
+
+<Highlight language={typescript} code={text} />
