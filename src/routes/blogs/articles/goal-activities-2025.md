0ab4a226f57f27e71cf6d2f8a39b7114e136dc50
fdiengott <freddy.diengott@proton.me>
2025-01-01 15:26:47 -0500

    add goal activities 2025 to blogs

---
+++ src/routes/blogs/articles/goal-activities-2025.md
@@ -0,0 +1,72 @@
+# Goal Activities 2025
+
+Lately, I've been listening to the podcast "[Philsophize This](https://www.philosophizethis.org/)" (shoutout to Stephen West!) fairly often, and the philosophical thought lately being discussed in the 20th century is how the goal of philosophy has gone from how one _should_ live, to how one _might_ live. So due to that, I've been thinking about what are things I can do to that pursuit that don't require a fundamental shift in my life.
+
+Which leads me to this short exercise. In NYC, it is frankly, overwhelming all of the things one can do. So this document/page is a list of a bunch of activities that I want to do, that are far beyond the simple going out to dinner or movie. It will hopefully grow overtime and I'll slowly add more detailed plans and places. But a living document such as this requires curiosity, mistakes, ambition, and collaboration.
+
+So here's to goals!
+
+## List of NYC activities in no particular order
+
+### Theater
+
+### Comedy
+
+### Jazz
+
+### Museum (tenement, poster, obviously art)
+
+### VR game place
+
+### Rollerskating rink (maybe ice skating too)
+
+### Axe throwing / archery
+
+### Video game day(s) (or bar)
+
+### Gallery - in Chelsea or elsewhere
+
+### Art fair (affordable or otherwise)
+
+### Piano time with my sweetie
+
+### Vocal improv
+
+### Spa day (or at least a schvitz or soak)
+
+### New clothes (ya girl hasn't historically liked shopping, but it could be fun in designer stores)
+
+### New furniture
+
+### Massage
+
+### Lectures / Classes / Talks (this is a large category that requires research)
+
+### Cooking class(es)
+
+### New vegan restaurants
+
+### Attend a full film festival
+
+### Babysitting
+
+### Attend Jewish services
+
+### Hiking upstate
+
+### Concerts
+
+This specifically includes:
+
+-   Groupmuses
+-   Caroline Shaw's performances
+
+### Host parties (dinner parties, or more chatting with acquaintances and their friends)
+
+### Dancing (mostly raves/clubs)
+
+### Watch documentaries
+
+### Escape room
+
+### Pickle ball / other sports
--- src/routes/blogs/articles/index.js
+++ src/routes/blogs/articles/index.js
@@ -9,68 +9,84 @@ import csvReact from './csv-react.md?raw';
 import staticFonts from './static-fonts.md?raw';
 import pdfReact from './pdf-react.md?raw';
 import recommendedBookList from './recommendedBookList';
+import goalActivities from './goal-activities-2025.md?raw';
+
+export const TAGS = {
+	suggestions: 'suggestions',
+	coding: 'coding',
+	recipes: 'recipes',
+	featured: 'featured',
+	musings: 'musings',
+	music: 'music',
+};
 
 const pages = {
+	'goal-activities-2025': {
+		title: 'Goal Activities 2025',
+		content: goalActivities,
+		date: '__TODAYS_DATE__',
+		tags: [TAGS.suggestions],
+	},
 	'book-list': {
 		title: 'Recent Reading List',
 		content: bookList,
 		date: '12/04/24',
-		tags: ['suggestions'],
+		tags: [TAGS.suggestions],
 		pinned: true,
 	},
 	'recommended-books': {
 		title: 'Recommended Book List',
 		content: recommendedBookList,
 		date: '12/04/24',
-		tags: ['suggestions'],
+		tags: [TAGS.suggestions],
 	},
 	'pdf-react': {
 		title: 'PDFs in React',
 		content: pdfReact,
 		date: '11/20/2024',
-		tags: ['coding'],
+		tags: [TAGS.coding],
 	},
 	'static-fonts': {
 		title: 'Where to Find Static Font Links',
 		content: staticFonts,
 		date: '11/6/2024',
-		tags: ['coding'],
+		tags: [TAGS.coding],
 	},
 	'csv-react': {
 		title: 'Downloading CSVs in React',
 		content: csvReact,
 		date: '11/2/2024',
-		tags: ['coding'],
+		tags: [TAGS.coding],
 	},
 	'blog-guide': {
 		title: 'Welcome to my blog!',
 		content: blogGuide,
 		date: '12/26/2023',
-		tags: ['featured'],
+		tags: [TAGS.featured],
 	},
 	regex101: {
 		title: 'Regex 101',
 		content: regex101,
 		date: '12/29/2023',
-		tags: ['coding'],
+		tags: [TAGS.coding],
 	},
 	'zsh-theme-customization': {
 		title: 'Zsh Theme Customization',
 		content: zshThemeCustomization,
 		date: '12/26/2023',
-		tags: ['coding'],
+		tags: [TAGS.coding],
 	},
 	// beginning: {
 	// 	title: 'It began with a whimper',
 	// 	content: beginning,
 	// 	date: '8/3/2023',
-	// 	tags: ['musings']
+	// 	tags: [TAGS.musings]
 	// },
 	'tomato-soup': {
 		title: 'The best vegan/gf tomato soup and grilled cheese recipe you can find',
 		content: tomatoSoup,
 		date: '8/2/2023',
-		tags: ['recipes'],
+		tags: [TAGS.recipes],
 	},
 	// ea: {
 	// 	title: 'Some recent musings on my personal philosophy and effective altruism',
