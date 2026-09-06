2674c2a95c161ec1f75a2ee4ae580b8125d2ef5a
fdiengott <freddy.diengott@proton.me>
2026-09-06 14:08:46 -0400

    add expecting better and the death of ivan ilych to booklist

--- src/routes/blogs/articles/bookList.js
+++ src/routes/blogs/articles/bookList.js
@@ -3,6 +3,8 @@ const bookList = `
 
 I've been reading mostly non-fiction lately, which is massively inspiring and exploding my thinking, adding new lenses to view the world, like a proper fox (as opposed to a hedgehog). I'd like to share some of the most recent books here that have made a big impact on me in chronological order, most recent first.
 
+- **The Death Of Ivan Ilyich** by Leo Tolstoy
+- **Expecting Better: Why the Conventional Pregnancy Wisdom Is Wrong – and What You Really Need to Know** by Emily Oster
 - **The Prize: The Epic Quest for Oil, Money, and Power** by Daniel Yergin
 - **Credit to the Nation: Eastern European Jewish Immigrant Bankers and the Shaping of American Finance, 1873-1930** by Rebecca Korbin
 - **How Africa Works: Success and Failure on the World's Last Developmental Frontier** by Joe Studwell
